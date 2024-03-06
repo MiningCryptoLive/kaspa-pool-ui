3ffc5953e82f09f1dcdaaaa9e729c3f1246dfc66
MiningCryptoLive <celldr69@gmail.com>
2024-03-06 09:25:50 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Nerd Pool</title>
+        <title>SOLOPOOL.US</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.png" />
       </Head>
